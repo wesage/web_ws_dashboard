7f8ee95210f206ba1492e724c365249e9e40f671
mm.chen <wesage@126.com>
2024-05-07 20:25:19 +0800

    更新版本信息,202405072025,ws_cmm

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'WSTRON文档中心'
-copyright = '2024, http://www.wstron.cn'
+copyright = '2024, WSTRON Inc'
 author = 'ws_cmm'
 
 # The full version, including alpha/beta/rc tags
